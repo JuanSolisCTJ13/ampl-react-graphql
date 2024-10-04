b2e379301cf051766c68a48dd5122b496c28df45
juansolisctj13 <juansolisctj@gmail.com>
2024-10-04 00:54:09 -0300

    2. agrega auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplreactgraphql2a4660f7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
